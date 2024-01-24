aebc539483a98ef35229ba71ebb403b9093f3d94
Aryan Yadav <yadavaryan2003@gmail.com>
2024-01-24 12:26:21 +0530

    SpeedometerColor

--- src/components/Speedometer.jsx
+++ src/components/Speedometer.jsx
@@ -1,36 +1,34 @@
 import ReactSpeedometer from "react-d3-speedometer";
-import React from 'react'
+import React from "react";
 
 const Speedometer = () => {
   return (
     <div className="bg-[#1C2438] flex flex-1   border border-gray-800 w-[15rem] h-[12rem] ml-5 mb-5 rounded-xl">
-        <div style={{
-    width: "15rem",
-    height: "192px",
-    background: "#1C2438",
-    display:"flex",
-    flex: "1 1 0%",
-    borderRadius:"5px",
-    
-
-}}>
-    <ReactSpeedometer
-        fluidWidth={false}
-        minValue={0}
-        maxValue={100}
-        value={86}
-        needleColor="steelblue"
-        segments={5}
-        // startColor=""
-        // endColor=""
-        ringWidth={10}
-        // textColor=""
-
-    />
+      <div
+        style={{
+          width: "15rem",
+          height: "192px",
+          background: "#1C2438",
+          display: "flex",
+          flex: "1 1 0%",
+          borderRadius: "5px",
+        }}
+      >
+        <ReactSpeedometer
+          fluidWidth={false}
+          minValue={0}
+          maxValue={100}
+          value={86}
+          needleColor="steelblue"
+          segments={5}
+          startColor="#793FDF"
+          endColor="#14C38E"
+          ringWidth={10}
+          // textColor=""
+        />
+      </div>
     </div>
-</div>
+  );
+};
 
-  )
-}
-
-export default Speedometer
+export default Speedometer;
