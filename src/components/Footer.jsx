cd1b4e731493354b48ccf349b14760fb1f80a1cb
Aditya <vinchhiaditya@gmail.com>
2024-01-24 12:47:42 +0530

    Footer Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer className="bg-slate-900 text-slate-200 px-20 py-1 ">
+            <div className='flex justify-center'>
+                <p>&#169; 2024 Digital Door. All right reserved.</p>
+            </div>
+        </footer>
+    )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,6 +4,7 @@ import Sidebar from "./Sidebar";
 import Header from "./Header";
 import Dashboard from "./Dashboard";
 import Map from "./Map";
+import Footer from "./Footer";
 
 const Layout = () => {
   const [map, setMap] = useState(null);
@@ -11,13 +12,12 @@ const Layout = () => {
     <>
       <div className="flex flex-row h-screen w-screen bg-[#F5F4F6] overflow-hidden">
         <Sidebar/>
-        
-       
         <div className="flex-1">
           <Header map={map}/>
           <Dashboard setMap={setMap}/>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
